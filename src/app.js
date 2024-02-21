809da23c8aef675332bc11d7989a3815247a7210
KarthikRam14 <karthikram1441@gmail.com>
2024-02-21 23:20:13 +0530

    done

--- src/app.js
+++ src/app.js
@@ -121,13 +121,61 @@ function filterByNoOfAwardsxTeamxAge(noOfAwards, team, age) {
   return data;
 }
 
+//Challenge 1 - Sort players in descending order of their age
+function SortByAge() {
+  for (let i = 0; i < players.length; i++) {
+    for (let j = i + 1; j < players.length; j++) {
+      if (players[i].age < players[j].age) {
+        let temp = players[i];
+        players[i] = players[j];
+        players[j] = temp;
+      }
+    }
+  }
+}
+//Challenge 2 - Sort players beloging to _____ team in descending order of awards won
 
-//Progression 9 - Sort players in descending order of their age
+function FilterByTeamxSortByNoOfAwards(team) {
+  let count = 0;
+  let filteredPlayers = [];
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].team == team) {
+      filteredPlayers[count++] = players[i];
+    }
+  }
+  for (let i = 0; i < filteredPlayers.length; i++) {
+    for (let j = i + 1; j < filteredPlayers.length; j++) {
+      if (filteredPlayers[i].awards.length < filteredPlayers[j].awards.length) {
+        let temp = filteredPlayers[i];
+        filteredPlayers[i] = filteredPlayers[j];
+        filteredPlayers[j] = temp;
+      }
+    }
+  }
+  return filteredPlayers;
+}
 
-//Progression 10 - Sort players beloging to _____ team in descending order of awards won
 
-//Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
 
-//Challenge 2 - Sort players that are older than _____ years in alphabetical order
-//Sort the awards won by them in reverse chronological order
+//Judgement 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
+function compare(a, b) {
+  let value = 0;
+  if (a.name > b.name) {
+    value = 1;
+  }
+  else if (a.name < b.name) { value = -1; }
+  else { value = 0; }
+}
+function SortByNamexAwardxTimes(awardName, noOfTimes, country) {
+  var dataOne = filterByAwardxTimes(awardName, noOfTimes);
+  var dataTwo = dataOne.filter(player => player.country == country);
+  return dataTwo.sort(compare);
+}
+
+
+//Judgement  2 - Sort players that are older than _____ years in alphabetical order
+function SortByNamexOlderThan(age) {
+  var data = players.filter(player => player.age > age);
+  return data;
+}
 
